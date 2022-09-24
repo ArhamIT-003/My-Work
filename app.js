d045ea2b43e4cd01a8fb807627ea2e9da3a8aeb4
Muhammad Arham <mak@gmail.com>
2022-09-24 14:44:56 +0500

    Added Google Authentication

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require ("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -32,30 +34,66 @@ mongoose.connect('mongodb://localhost:27017/userDB');
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
 /// Initalize the plugin for passportLocalMongoose //
 userSchema.plugin(passportLocalMongoose);
+// plugin for findOrCreate //
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 /// for session //
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+//// Google Strategy ////
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 ////     Get Route . ///
 app.get("/", (req,res)=>{
   res.render("home");
 });
+
+app.get('/auth/google',
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
+
 app.get("/login", (req,res)=>{
   res.render("login");
 });
+
 app.get("/register", (req,res)=>{
   res.render("register");
 });
+
 app.get("/secrets", (req, res)=>{
 
   if(req.isAuthenticated()){               /// request is authenticate //
